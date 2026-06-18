0dc26af7ee2451c26733f4bcfc1958f6c0aeac04
kumarang9 <105969507+kumarang9@users.noreply.github.com>
2026-06-18 16:15:35 +0530

    appupdate

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,12 @@
-FROM python:3.11-slim
+FROM python:3.11
 
 WORKDIR /app
 
 COPY . .
 
-RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install -r requirements.txt
 
 EXPOSE 5000
 
-CMD ["gunicorn","--bind","0.0.0.0:5000","app:app","python","app.py"]
+CMD ["python","app.py"]
+
--- app.py
+++ app.py
@@ -1,34 +1,12 @@
 from flask import Flask
+import psycopg2
 import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-
-    db_host = os.getenv("DB_HOST", "Not Configured")
-    db_user = os.getenv("DB_USER", "Not Configured")
-
-    return f"""
-    <h1>Application Running Successfully</h1>
-
-    <h2>AWS EKS Demo Application</h2>
-
-    <p><b>Database Host:</b> {db_host}</p>
-
-    <p><b>Database User:</b> {db_user}</p>
-
-    <p><b>Environment:</b> Production</p>
-    """
-
-@app.route("/health")
-def health():
-    return {
-        "status": "UP"
-    }
+    return "Application deployed successfully"
 
 if __name__ == "__main__":
-    app.run(
-        host="0.0.0.0",
-        port=5000
-    )
+    app.run(host="0.0.0.0",port=5000)
